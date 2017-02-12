3062954b87ec1d495fe126b51ea5e91ec34df29d
Loris Guerra <loris4526@gmail.com>
2017-02-13 00:02:09 +0100

    Add axios and make http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,39 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
+import axios from "axios";
 
 import AlbumDetail from "./AlbumDetail";
 
 export default class AlbumList extends Component {
 
+  constructor(props){
+    super(props);
+    this.state= {
+      albums: []
+    }
+  }
+
+  componentWillMount(){
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((res)=>{
+        this.setState({
+          albums: res.data
+        });
+      })
+  }
+
+  componentDidUpdate(prevProps,prevState){
+    if(this.state.albums.length>0){
+      console.log('there are some albums', this.state.albums);
+    } else {
+      console.log('no albums yet')
+    }
+  }
+
   render(){
     return(
       <View>
         <Text>AlbumList</Text>
-        <AlbumDetail/>
       </View>
     )
   }
